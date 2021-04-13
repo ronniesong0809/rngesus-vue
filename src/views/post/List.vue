399083c31ee94cddd13bd8a54e844a5971752e08
Ronnie Song <ronniesong0809@gmail.com>
2021-04-12 21:20:58 -0700

    add tab page to display latest and hot posts

--- src/views/post/List.vue
+++ src/views/post/List.vue
@@ -1,62 +1,35 @@
 <template>
   <el-card>
     <div class="clearfix" slot="header">
-      <article class="media" v-for="(post, index) in records" :key="index">
-        <div class="media-left">
-          <figure class="image is-48x48">
-            <img
-              :src="`https://www.gravatar.com/avatar/${post.avatar}`"
-              style="border-radius: 10px;"
-            />
-          </figure>
-        </div>
-        <div class="media-content">
-          <div class="content-title">
-            <p class="ellipsis is-ellipsis-1">
-              <span class="is-size-6">{{ post.title }}</span>
-            </p>
-          </div>
-          <nav class="level has-text-grey is-mobile is-size-7 mt-3">
-            <div class="level-left">
-              <span class="displayName has-text-black mr-1">
-                {{ post.alias || post.userName }}
-              </span>
-              <span class="createTime mr-1">
-                {{ dayjs(post.createTime).format("YYYY/MM/DD") }}
-              </span>
-
-              <span
-                class="tag is-hidden-mobile is-danger is-light mr-1"
-                v-for="(tag, index) in post.tags"
-                :key="index"
-              >
-                {{ tag.name }}
-              </span>
-
-              <span class="views is-hidden-mobile">
-                views: {{ post.view }}
-              </span>
-            </div>
-          </nav>
-        </div>
-        <div class="media-right" />
-      </article>
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="New" name="latest" icon="">
+          <Post :records="records" />
+        </el-tab-pane>
+        <el-tab-pane label="Hot" name="hottest">
+          <Post :records="records" />
+        </el-tab-pane>
+      </el-tabs>
     </div>
   </el-card>
 </template>
 
 <script>
 import { getList } from "@/api/post";
+import Post from "@/views/post/Post";
 
 export default {
   name: "List",
   data() {
     return {
-      records: []
+      records: [],
+      activeName: "latest"
     };
   },
+  components: {
+    Post
+  },
   created() {
-    this.init("lasted");
+    this.init(this.tab);
   },
   methods: {
     init(tab) {
@@ -64,6 +37,9 @@ export default {
         const { data } = response;
         this.records = data.records;
       });
+    },
+    handleClick(tab) {
+      this.init(tab.name);
     }
   }
 };
---
+++ src/views/post/Post.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <article class="media" v-for="(post, index) in records" :key="index">
+      <div class="media-left">
+        <figure class="image is-48x48">
+          <img
+            :src="`https://www.gravatar.com/avatar/${post.avatar}`"
+            style="border-radius: 10px;"
+          />
+        </figure>
+      </div>
+      <div class="media-content">
+        <div class="content-title">
+          <p class="ellipsis is-ellipsis-1">
+            <span class="is-size-6">{{ post.title }}</span>
+          </p>
+        </div>
+        <nav class="level has-text-grey is-mobile is-size-7 mt-3">
+          <div class="level-left">
+            <span class="displayName has-text-black mr-1">
+              {{ post.alias || post.userName }}
+            </span>
+            <span class="createTime mr-1">
+              {{ dayjs(post.createTime).format("YYYY/MM/DD") }}
+            </span>
+
+            <span
+              class="tag is-hidden-mobile is-danger is-light mr-1"
+              v-for="(tag, index) in post.tags"
+              :key="index"
+            >
+              {{ tag.name }}
+            </span>
+
+            <span class="views is-hidden-mobile">views: {{ post.view }}</span>
+          </div>
+        </nav>
+      </div>
+      <div class="media-right" />
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Post",
+  props: ["records"]
+};
+</script>
